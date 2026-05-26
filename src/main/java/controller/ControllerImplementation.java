3c8659c49302dd9919c146ec2f1d1519d2b98adb
IvanFP <iivanfp29@gmail.com>
2026-05-26 10:54:46 +0200

    Show Confirmation and Information Panel for Deleting a Person

--- src/main/java/controller/ControllerImplementation.java
+++ src/main/java/controller/ControllerImplementation.java
@@ -273,9 +273,25 @@ public class ControllerImplementation implements IController, ActionListener {
 
     public void handleDeletePerson() {
         if (delete != null) {
+            int confirm = JOptionPane.showConfirmDialog(
+                    null,
+                    "Are you sure you want to delete this person?",
+                    "Delete - People vl.1.0",
+                    JOptionPane.YES_NO_OPTION,
+                    JOptionPane.QUESTION_MESSAGE
+            );
+            
+        if (confirm == JOptionPane.YES_OPTION) {
             Person p = new Person(delete.getNif().getText());
             delete(p);
+            JOptionPane.showMessageDialog(
+                    null,
+                    "Person deleted succesfully!",
+                    "Delete - People v1.1.0",
+                    JOptionPane.INFORMATION_MESSAGE
+            );
             delete.getReset().doClick();
+            }
         }
     }
 
